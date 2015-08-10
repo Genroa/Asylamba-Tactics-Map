78a2d542c4b990f4bcb63c21bfcfb2bc73ce78a5
Genroa <genroa@hotmail.fr>
2015-08-10 21:11:35 +0200

    first commit

---
+++ Asylamba_Tactics_Map.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Asylamba's Tactics Map
+// @namespace   asylamba
+// @description Userscript pour l'ajout de dessins tactiques directement sur la map d'Asylamba
+// @include     http://game.asylamba.com/*/map*
+// @match       http://game.asylamba.com/*/map*
+// @grant       GM_xmlhttpRequest
+// @version     0.0.1
+// @grant       Genroa
+// @author      Genroa
+// @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// @require		https://cdnjs.cloudflare.com/ajax/libs/interact.js/1.2.4/interact.min.js
+// ==/UserScript==
+
