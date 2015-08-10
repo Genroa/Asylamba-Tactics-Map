d700522a0c91d6b20429628ef51637b9dc35855d
Genroa <genroa@hotmail.fr>
2015-08-10 21:16:52 +0200

    first tries

--- Asylamba_Tactics_Map.user.js
+++ Asylamba_Tactics_Map.user.js
@@ -6,10 +6,17 @@
 // @match       http://game.asylamba.com/*/map*
 // @grant       GM_xmlhttpRequest
 // @updateURL   https://github.com/Genroa/Asylamba-Tactics-Map/raw/master/Asylamba_Tactics_Map.user.js
-// @version     0.0.1
+// @version     0.0.2
 // @grant       Genroa
 // @author      Genroa
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @require		https://cdnjs.cloudflare.com/ajax/libs/interact.js/1.2.4/interact.min.js
 // ==/UserScript==
 
+$(function(){
+	$('#map').prepend('<div id="ATM" style="width: 100%; height: 100%; position: absolute;"></div>');
+
+	$(document).on('click', function(){
+
+	});
+});
